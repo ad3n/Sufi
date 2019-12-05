5ab23e70867945d5018ae9a686a8d48f66a15ca2
aden <surya.kejawen@gmail.com>
2019-12-05 18:38:34 +0700

    add public access

--- src/Installment/InstallmentService.php
+++ src/Installment/InstallmentService.php
@@ -137,7 +137,7 @@ class InstallmentService implements ServiceInterface
      */
     public function getByOrder(Order $order): array
     {
-        return $this->installmentRepository->findBy(['order' => $order]);
+        return $this->installmentRepository->findBy(['order' => $order], ['installmentDate' => 'DESC']);
     }
 
     /**
